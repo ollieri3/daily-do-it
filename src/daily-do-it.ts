c3ced4f57c7d753124bc6b9cd67e1f35c4f9e178
ollieri3 <oliver.ridsdale@gmail.com>
2023-08-24 20:14:24 +0100

    Improve homepage styling

--- src/daily-do-it.ts
+++ src/daily-do-it.ts
@@ -216,8 +216,30 @@ app.get("/", (req, res) => {
     res.redirect("/calendar");
     return;
   }
+
+  // For generating the calendar animation
+  let daysCount = 0;
+  const months = dayjs.monthsShort().map((month, index) => {
+    const today = dayjs();
+    const currentYear = dayjs().get("year");
+    const monthDate = dayjs(new Date(currentYear, index));
+    const numberOfDays = monthDate.daysInMonth();
+    return {
+      month,
+      days: [...Array(numberOfDays).keys()].map((i) => {
+        return {
+          day: i + 1,
+          animationOrder: daysCount++,
+          isComplete: today.isAfter(dayjs(new Date(currentYear, index, i + 1))),
+        };
+      }),
+    };
+  });
+  console.dir(months, { depth: null });
+  // console.log("daysCount: ", daysCount);
   res.render("home", {
     username: (req.user as any)?.email,
+    months,
   });
 });
 
