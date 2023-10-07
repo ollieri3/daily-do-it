06f699808fd944069c709add1907b9c343a6b7e0
ollieri3 <oliver.ridsdale@gmail.com>
2023-10-07 21:33:07 +0100

    Add basic signin test

---
+++ tests/signin.spec.ts
@@ -0,0 +1,22 @@
+// These are local DB credentials only
+//TODO: Move these to be dynamic based on some scaffolded data
+const credentials = {
+  email: "foo@bar.com",
+  password: "123456789",
+};
+
+import { test, expect } from "@playwright/test";
+
+test("Sign in with Email & Password", async ({ page }) => {
+  await page.goto("/signin");
+
+  await page.getByLabel("Email").click();
+  await page.keyboard.type(credentials.email);
+
+  await page.getByLabel("Password").click();
+  await page.keyboard.type(credentials.password);
+
+  await page.getByRole("button", { name: "Sign in" }).click();
+
+  await expect(page).toHaveTitle("Your Calendar - Daily Do It");
+});
